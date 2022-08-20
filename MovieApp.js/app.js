c4106121d6fa3a4796b8d782a61fda9ad859e3fa
Yau1heni <lodbrok9@gmail.com>
2022-08-20 16:45:16 +0300

    implemented search capability

--- MovieApp.js/app.js
+++ MovieApp.js/app.js
@@ -1,13 +1,16 @@
 const API_KEY = '4d4720a3-f248-4b63-8078-9a6a932e897f'
 const API_URL_POPULAR =
     'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
+const API_URL_SEARCH =
+    'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
 
 getMovies(API_URL_POPULAR)
 
 function getClassByRate(rating) {
     if (rating >= 7) return "green"
-    else if (rating > 5) return "orange"
-    else return "red"
+    else if (rating >= 5) return "orange"
+    else if (rating < 5) return "red"
+    else return "no-rating"
 }
 
 async function getMovies(url) {
@@ -40,3 +43,16 @@ function showMovies(data) {
         moviesEl.appendChild(movieEl)
     });
 }
+
+const form = document.querySelector("form");
+const search = document.querySelector(".header__search");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
+    if (search.value) {
+        getMovies(apiSearchUrl)
+        search.value = ''
+        document.querySelector(".movies").innerHTML = "";
+    }
+})
