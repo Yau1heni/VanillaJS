762f9a9af0519bfc81cbfa98ed1e2d1c1ff66048
Yau1heni <lodbrok9@gmail.com>
2022-08-23 22:22:52 +0300

    Basic Data Structures. Сompleted

--- freeCodeCamp/Basic Data Structures/app.js
+++ freeCodeCamp/Basic Data Structures/app.js
@@ -87,3 +87,169 @@ let myNestedArray = [
     ['mutate', 1327.98, 'splice', 'slice', 'push', [['deeper']]],
     ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', [[['deepest']]]]
 ]
+
+//13. Add Key-Value Pairs to JavaScript Objects
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+}
+
+foods['bananas'] = 13
+foods['grapes'] = 35
+foods['strawberries'] = 27
+
+console.log(foods)
+
+//14. Modify an Object Nested Within an Object
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+        totalUsers: 51,
+        online: 42
+    }
+}
+
+userActivity.data.online = 45
+
+console.log(userActivity)
+
+//15. Access Property Names with Bracket Notation
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+}
+
+function checkInventory(scannedItem) {
+    return foods[scannedItem]
+}
+
+console.log(checkInventory("apples"))
+
+//16. Use the delete Keyword to Remove Object Properties
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+}
+
+delete foods.oranges
+delete foods.plums
+delete foods.strawberries
+
+console.log(foods)
+
+//17. Check if an Object has a Property
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(userObj) {
+    return (userObj.hasOwnProperty('Alan')
+        && userObj.hasOwnProperty('Jeff')
+        && userObj.hasOwnProperty('Sarah')
+        && userObj.hasOwnProperty('Ryan'))
+}
+
+console.log(isEveryoneHere(users))
+
+//18. Iterate Through the Keys of an Object with a for...in Statement
+const users2 = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+}
+
+function countOnline(usersObj) {
+    let onlineCount = 0
+    for (let user in usersObj) {
+        if (usersObj[user].online === true) onlineCount++
+    }
+    return onlineCount
+}
+
+console.log(countOnline(users2))
+
+//19. Generate an Array of All Object Keys with Object.keys()
+let users3 = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function getArrayOfUsers(obj) {
+    return Object.keys(obj)
+}
+
+console.log(getArrayOfUsers(users3))
+
+//20. Modify an Array Stored in an Object
+let user4 = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+};
+
+function addFriend(userObj, friend) {
+    let arrFriends = userObj.data.friends
+    arrFriends.push(friend)
+    return arrFriends
+}
+
+console.log(addFriend(user, 'Pete'))
